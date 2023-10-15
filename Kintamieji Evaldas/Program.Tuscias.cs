ef55a33dab5903318eeb2f1aa809f42ebc26ebc3
Efka&Herkus <Efka&Herkus@DESKTOP-PQLS3QK>
2023-10-16 00:07:16 +0300

    Add project files.

---
+++ Kintamieji Evaldas/Program.Tuscias.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kintamieji_Evaldas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string Vardas = "Evaldas";
+            string Pavardė = "Lekavičius";
+            int Amžius = 45;
+            double Ūgis = 175,5;
+            double Svoris = 86.6;
+            string AukŠtojiMokykla = "Kauno Technologijos Universitetas";
+            string AkademinėsGrupėsKodas = "TEST2023";
+            int Kursas = 4;
+            string Studij7ProgramosPavadinimas = "Testavimas";
+            int AtsiskaitytųKreditųSkaičius = 80;
+
+        }
+    }
+}
